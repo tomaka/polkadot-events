6b9d85235394c28d9851890affba892c219acf55
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-03-13 12:21:21 +0100

    Switch to Polkadot

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { default as App } from './components/App.jsx';
 
-import polkadot_chains_specs from './westend.json'; // TODO: should be polkadot
+import polkadot_chains_specs from './polkadot.json';
 
 ReactDom.render(
     <App chainSpec={polkadot_chains_specs} />,
